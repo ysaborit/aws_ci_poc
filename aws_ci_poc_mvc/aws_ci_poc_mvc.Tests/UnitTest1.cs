873486ebee440942dbcb64f283fb843f7a6d9818
Milene Garcia <mileneggarci@gmail.com>
2018-03-23 11:50:45 -0400

    Initial commit II

---
+++ aws_ci_poc_mvc/aws_ci_poc_mvc.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace aws_ci_poc_mvc.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ aws_ci_poc_mvc/aws_ci_poc_mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aws_ci_poc_mvc.MvcApplication" Language="C#" %>
